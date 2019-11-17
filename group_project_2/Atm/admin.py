d328d0169e9fa3e95fb016704327096dfe5271f8
Peter Keres <keres_peter@columbusstate.edu>
2019-11-17 15:20:14 -0500

    added the amt card model to database

--- group_project_2/Atm/admin.py
+++ group_project_2/Atm/admin.py
@@ -3,5 +3,7 @@
 from django.contrib import admin
 
 from .models import Account_Extension
+from .models import Atm_Card
 
 admin.site.register(Account_Extension)
+admin.site.register(Atm_Card)
--- group_project_2/Atm/models.py
+++ group_project_2/Atm/models.py
@@ -23,6 +23,7 @@ holds:
     currnet balance of the account
 '''
 class Account_Extension(models.Model):
+    #account number = self.id | is defualt build into the modles
     name = models.CharField(max_length = 200)
     phone_number = models.CharField(max_length = 15)
     balance = models.DecimalField(max_digits=19, decimal_places = 2)
@@ -34,4 +35,27 @@ class Account_Extension(models.Model):
     how each record wil be displayed.
     '''
     def __str__(self):
-        return str(self.id) + " : " + self.name
+        return "Account number is : " + str(self.id) + " | Owner is : " + self.name
+
+
+'''
+
+'''
+class Atm_Card(models.Model):
+    #atm_account_number = self.id | is defualt build into the modles
+    account_number = models.ForeignKey('Account_Extension', on_delete = models.PROTECT)
+    pin = models.CharField(max_length = 4)
+    name = models.CharField(max_length = 200)
+    date_issued = models.DateField(null = True, auto_now_add = True)
+    data_expire = models.DateField(null = True)
+    address = models.CharField(max_length = 200, default = "NA")
+
+    twof_status_choices = [ ("Active","Active") , ("Inactive","Inactive") ]
+    two_factor_status = models.CharField(max_length = 10, default = "Inactive", choices = twof_status_choices)
+
+    Card_status_choices = [ ("Active","Active") , ("Inactive","Inactive") ]
+    card_status = models.CharField(max_length = 10, default = "Inactive", choices = Card_status_choices)
+
+
+    def __str__(self):
+        return "Atm card number is : " + str(self.id)
