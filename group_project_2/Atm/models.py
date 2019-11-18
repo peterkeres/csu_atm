3893ba30c3f51d7bc49497c304dd9e4e33bd9e06
Darian Thomas <darian.thomas61@gmail.com>
2019-11-18 01:30:51 -0500

    syntax

--- group_project_2/Atm/models.py
+++ group_project_2/Atm/models.py
@@ -73,7 +73,7 @@ transfer_processStatus = (0, 'Initiated'), (1, 'Did not go through')
 class Transaction(models.Model):
     account_numberRecipient = models.CharField(max_length=12)
     account_numberSender = models.CharField(max_length=12)
-    amount = models.DecimalField(max_digits=20, decial_places=2)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
     status = models.IntegerField(choices=transaction_Statuses, default=0)
 
     createAt = models.DateTimeField(default=timezone.now)
